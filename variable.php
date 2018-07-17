1068f9f7dcf131f1846165acd19c5da90bf37ce7
shindeajay170@gmail.com <shindeajay170@gmail.com>
2018-07-17 13:35:21 +0200

    variables exercices

---
+++ variable.php
@@ -0,0 +1,39 @@
+<?php
+if($hungry_or_not)
+   {
+       echo "I 'm hungry";
+   }
+   else{
+       echo "I' m not hungry";
+   }
+   ?>
+
+   <?php
+   for ($family = 0; $family < 3; $family++);
+   
+   ?>
+<!DOCTYPE html>
+<html>
+  <head><title>Hi!</title></head>
+  <body>
+   <?php
+    $first_name = "Ajay";
+     echo $first_name;
+   ?>
+   <?php
+    $hungry_or_not = false;
+     echo $hungry_or_not;
+   ?>
+   <?php
+    $eye_color = "black";
+    echo $eye_color;
+   ?>
+   <?php
+  
+    
+   $family = array("Child1", "Child2", "My_wife", "Me");
+   echo print_r($family);
+   ?>
+ 
+  </body>
+</html>
