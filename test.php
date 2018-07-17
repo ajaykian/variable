cf0cd0c8d30888bf81d2ea2b98893c65ae61494c
shindeajay170@gmail.com <shindeajay170@gmail.com>
2018-07-17 16:40:43 +0200

    exercices conditions

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+// $sale = $_POST['sale'];
+// if ($sale) {
+//     echo "sale sale";
+// }
+?>
+
+<!-- <form action="" method="post"><input type="submit" value="sale" name="sale"></form> -->
